6535566ebfd16f22dda50caa55c92a7d28d437b1
samridhi garg <samridhigarg2k@gmail.com>
2022-08-19 08:50:49 +0530

    Time: 103 ms (89.08%), Space: 58.9 MB (80.58%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -4,25 +4,34 @@ public:
         int n=height.size();
         int maxi=-1e9;
         // vector<int> left(n),right(n);
-        int left,right;
-        for(int i=0;i<n;i++){
-            int l=0,r=n-1;
-            left=i,right=i;
-            while(l<i){
-                if(height[l]>=height[i]){
-                    left=l;
-                    break;
-                }
-                l++;
+        // int left,right;
+        // for(int i=0;i<n;i++){
+            int i=0,j=n-1;
+        //     left=i,right=i;
+        //     while(l<i){
+        //         if(height[l]>=height[i]){
+        //             left=l;
+        //             break;
+        //         }
+        //         l++;
+        //     }
+        //     while(r>i){
+        //         if(height[r]>=height[i]){
+        //             right=r;
+        //             break;
+        //         }
+        //         r--;
+        //     }
+        //     maxi=max(maxi,height[i]*(right-left));
+        // }
+        while(i<j){
+            maxi=max(maxi,(j-i)*min(height[i],height[j]));
+            if(height[i]<height[j]){
+                i++;
             }
-            while(r>i){
-                if(height[r]>=height[i]){
-                    right=r;
-                    break;
-                }
-                r--;
+            else{
+                j--;
             }
-            maxi=max(maxi,height[i]*(right-left));
         }
         return maxi;
     }
