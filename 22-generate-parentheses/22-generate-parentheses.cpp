c1f5d2817274a8d8a35879d01e2129e67a769ab0
samridhi garg <samridhigarg2k@gmail.com>
2022-08-19 17:23:50 +0530

    Time: 3 ms (87.36%), Space: 6.8 MB (99.98%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -20,11 +20,26 @@ class Solution {
         temp[ind]=')';
         add_parenthesis(temp,res,ind+1,m);
     }
+    void optimal(string& temp,vector<string>& res,int open,int close,int ind,int n){
+        if(ind==2*n){
+            res.push_back(temp);
+            return;
+        }
+        if(open<n){
+            temp[ind]='(';
+            optimal(temp,res,open+1,close,ind+1,n);
+        }
+        if(close<open){
+            temp[ind]=')';
+            optimal(temp,res,open,close+1,ind+1,n);
+        }
+    }
 public:
     vector<string> generateParenthesis(int n) {
         vector<string> res;
         string temp(2*n,'.');
-        add_parenthesis(temp,res,0,n);
+        // add_parenthesis(temp,res,0,n);
+        optimal(temp,res,0,0,0,n);
         return res;
     }
 };
