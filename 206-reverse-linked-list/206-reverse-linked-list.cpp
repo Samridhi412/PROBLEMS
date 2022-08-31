765e065393936817a00f8b38e154afbf8187785d
samridhi garg <samridhigarg2k@gmail.com>
2022-08-31 19:34:17 +0530

    Time: 6 ms (85.19%), Space: 8.6 MB (9.97%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -9,16 +9,27 @@
  * };
  */
 class Solution {
+    ListNode* recur(ListNode* head){
+        if(!head||!head->next){
+            return head;
+        }
+        ListNode* node=recur(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return node;
+        
+    }
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev=NULL,*next=NULL;
-        ListNode* curr=head;
-        while(curr){
-            next=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=next;
-        }
-        return prev;
+        // ListNode* prev=NULL,*next=NULL;
+        // ListNode* curr=head;
+        // while(curr){
+        //     next=curr->next;
+        //     curr->next=prev;
+        //     prev=curr;
+        //     curr=next;
+        // }
+        // return prev;
+        return recur(head);
     }
 };
