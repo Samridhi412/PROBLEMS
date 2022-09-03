059522a7da4b63b507889868e7f0f545fe6db574
samridhi garg <samridhigarg2k@gmail.com>
2022-09-03 13:37:12 +0530

    Time: 45 ms (65.67%), Space: 13.1 MB (78.17%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -8,18 +8,38 @@ public:
         if(k>n*n)return -1;
         
         
-        auto cmp=[&](pii& a,pii& b){
-             return matrix[a.first][a.second]>matrix[b.first][b.second];
-        };
-        priority_queue<pii,vector<pii>,decltype(cmp)> pq(cmp);
-        for(int i=0;i<n;i++){
-        pq.push({i,0});
+//         auto cmp=[&](pii& a,pii& b){
+//              return matrix[a.first][a.second]>matrix[b.first][b.second];
+//         };
+//         priority_queue<pii,vector<pii>,decltype(cmp)> pq(cmp);
+//         for(int i=0;i<n;i++){
+//         pq.push({i,0});
+//         }
+//         for(int i=1;i<k;i++){
+//         pii f=pq.top();
+//         pq.pop();
+//         if(f.second+1<n)pq.push({f.first,f.second+1});
+//         }
+//         return matrix[pq.top().first][pq.top().second];
+        
+        //BINARY SEARCH
+        int low=matrix[0][0];
+        int high=matrix[n-1][n-1];
+        while(low<=high){
+            int mid=(low+high)>>1;
+            int ans=0;
+            for(int i=0;i<n;i++){
+                int pos=upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin();
+                ans+=pos;
+                
+            }
+            if(ans<k){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
         }
-        for(int i=1;i<k;i++){
-        pii f=pq.top();
-        pq.pop();
-        if(f.second+1<n)pq.push({f.first,f.second+1});
-        }
-        return matrix[pq.top().first][pq.top().second];
+        return low;
     }
 };
