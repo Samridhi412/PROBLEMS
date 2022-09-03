83d222761cecd98ca375ff6ef36567c255014e60
samridhi garg <samridhigarg2k@gmail.com>
2022-09-03 12:23:28 +0530

    Time: 14 ms (26.43%), Space: 9.9 MB (93.03%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int> mp;
-        for(int i=0;i<nums1.size();i++){
-            mp[nums1[i]]++;
-        }
+        // unordered_map<int,int> mp;
+        // for(int i=0;i<nums1.size();i++){
+        //     mp[nums1[i]]++;
+        // }
         vector<int> res;
-        for(int i=0;i<nums2.size();i++){
-            unordered_map<int,int>::iterator it=mp.find(nums2[i]);
-            if(it!=mp.end()&&it->second>0){
-                res.push_back(nums2[i]);
-                it->second--;
+        // for(int i=0;i<nums2.size();i++){
+        //     unordered_map<int,int>::iterator it=mp.find(nums2[i]);
+        //     if(it!=mp.end()&&it->second>0){
+        //         res.push_back(nums2[i]);
+        //         it->second--;
+        //     }
+        // }
+        // return res;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int i=0,j=0;
+        while(i<nums1.size()&&j<nums2.size()){
+            if(nums1[i]==nums2[j]){
+                res.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else{
+                j++;
             }
         }
         return res;
