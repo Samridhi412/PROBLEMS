72e50b85930a273eae42d26ae6ceaf87e6e3dc9f
samridhi garg <samridhigarg2k@gmail.com>
2022-09-03 16:52:06 +0530

    Time: 5 ms (67.22%), Space: 6.2 MB (95.93%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -27,12 +27,25 @@ class Solution {
         }
         return end-start;
     }
+    int getuniquecount(string& s){
+        bool mp[26];
+        memset(mp,0,sizeof(mp));
+        int uniq=0;
+        for(int i=0;i<s.size();i++){
+            if(mp[s[i]-'a']==0){
+                uniq++;
+                mp[s[i]-'a']=1;
+            }
+            
+        }
+        return uniq;
+    }
 public:
     int longestSubstring(string s, int k) {
         //BRITE FORCE
         int n=s.size();
-        // int maxlen=0;
-        // if(n<k||n==0)return 0;
+        int maxlen=0;
+        if(n<k||n==0)return 0;
         // int count_chars[26]={0};
         // for(int i=0;i<n;i++){
         //     memset(count_chars,0,sizeof(count_chars));
@@ -48,6 +61,44 @@ public:
         
         
         //DIVIDE AND CONQUER
-        return helper(s,k,0,n);
+        // return helper(s,k,0,n);
+        
+        //sliding window
+        int maxunique=getuniquecount(s);
+        // cout<<maxunique<<endl;
+        int count[26];
+        for(int curruniq=1;curruniq<=maxunique;curruniq++){
+            memset(count,0,sizeof(count));
+            int startwindow=0,endwindow=0;
+            int uniq=0,cntk=0;
+            while(endwindow<n){
+                if(uniq<=curruniq){
+                    int idx=s[endwindow]-'a';
+                    if(count[idx]==0){
+                        uniq++;
+                    }
+                    count[idx]++;
+                    if(count[idx]==k){
+                        cntk++;
+                    }
+                    endwindow++;
+                }
+                else{
+                    int idx=s[startwindow]-'a';
+                    if(count[idx]==k){
+                        cntk--;
+                    }
+                    count[idx]--;
+                    if(count[idx]==0){
+                        uniq--;
+                    }
+                    startwindow++;
+                }
+                if(uniq==curruniq&&uniq==cntk){
+                    maxlen=max(maxlen,endwindow-startwindow);
+                }
+            }
+        }
+        return maxlen;
     }
 };
