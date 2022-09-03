ba240e9669e2e8b98c87ea6600c5cc29f023935f
samridhi garg <samridhigarg2k@gmail.com>
2022-09-03 11:41:12 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.77%) - LeetHub

---
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int getSum(int a, int b) {
+        int sum=0,c=0;
+        for(int i=0;i<32;i++){
+            int x=a&1;
+            int y=b&1;
+            a=a>>1;
+            b=b>>1;
+            sum=sum|(((x^y)^c)<<i);
+            c=(x&y|c&x|c&y);
+        }
+        return sum;
+    }
+};
+
+// class Solution {
+// public:
+//     int getSum(int a, int b) {
+//         int sum = 0, c = 0; // 'c' represents our carry bit
+//         int x = 0, y = 0;
+//         for (int i=0; i<32; i++) {
+//             x = 1 & a; a = a >> 1; 
+//             y = 1 & b; b = b >> 1;
+//             sum = sum | (((x ^ y) ^ c) << i);
+//             c = (x&y | y&c | c&x);
+//         }
+//         return sum;
+//     }
+// };
