9f55bafbd4be41acfa56900cc1183edd6d7fe88b
samridhi garg <samridhigarg2k@gmail.com>
2022-09-03 14:54:04 +0530

    Time: 168 ms (5.20%), Space: 57.1 MB (28.91%) - LeetHub

--- 384-shuffle-an-array/384-shuffle-an-array.cpp
+++ 384-shuffle-an-array/384-shuffle-an-array.cpp
@@ -1,50 +1,47 @@
-
-// class Solution {
-// public:
+class Solution {
+public:
+    vector<int> original,temp;
     
-//     vector<int> v,nums;
-//     Solution(vector<int>& vec) {
-//         v = vec;
-//         nums=vec;
+    Solution(vector<int>& nums) {
+        original=nums;
+        temp=nums;
+    }
+    
+    vector<int> reset() {
+        return original;
+    }
+    
+    vector<int> shuffle() {
+        int n=temp.size();
+        for(int i=n-1;i>0;i--){
+            int j=rand()%(i+1);
+            swap(temp[i],temp[j]);
+        }
+        return temp;
+    }
+};
+// class Solution {
+//     vector<int> nums,v;
+// public:
+//     Solution(vector<int>& nums1) {
+//         this->nums=nums1;
+//         this->v=nums1;
 //     }
     
 //     vector<int> reset() {
+//         // nums=v;
 //         return v;
 //     }
     
 //     vector<int> shuffle() {
-        
-//         int n = v.size();
-//         int random = rand() % n;
-
+//         int n=v.size();
+//         int idx2=rand()%n;
 //         static int i=0;
-//         swap(nums[i%n],nums[random]);
+//         swap(nums[i%n],nums[idx2]);
 //         i++;
 //         return nums;
 //     }
 // };
-class Solution {
-    vector<int> nums,v;
-public:
-    Solution(vector<int>& nums1) {
-        this->nums=nums1;
-        this->v=nums1;
-    }
-    
-    vector<int> reset() {
-        // nums=v;
-        return v;
-    }
-    
-    vector<int> shuffle() {
-        int n=v.size();
-        int idx2=rand()%n;
-        static int i=0;
-        swap(nums[i%n],nums[idx2]);
-        i++;
-        return nums;
-    }
-};
 
 /**
  * Your Solution object will be instantiated and called as such:
